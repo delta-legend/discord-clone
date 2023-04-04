02e6a3d331a63df4907150427df2b16461b1d2f9
delta-legend <writetokartiktyagi@gmail.com>
2023-04-04 09:01:13 +0530

    add platform grid component

--- components/PlatformsGrid.js
+++ components/PlatformsGrid.js
@@ -1,3 +1,56 @@
+import styles from "../styles/platform.module.css";
+
 export default function PlatformGrid() {
-  return <div>Platform Grid</div>;
+  return (
+    <div>
+      <div className={styles.container}>
+        {/* iOS Box */}
+        <div className={styles.item1}>
+          <div className={styles.ios}>
+            <div className={styles.iosText}> iOS</div>
+            <br />
+            <a className={styles.iosDownload}>Download</a>
+            <div>
+              <img
+                className={styles.iosImage}
+                src="ios.svg"
+                alt="ios-image"
+              ></img>
+            </div>
+          </div>
+        </div>
+        {/* Android box */}
+        <div className={styles.item2}>
+          <div className={styles.android}>
+            <div className={styles.androidText}> Android</div>
+            <br />
+            <a className={styles.androidDownload}>Download</a>
+            <div>
+              <img
+                className={styles.androidImage}
+                src="ios.svg"
+                alt="ios-image"
+              ></img>
+            </div>
+          </div>
+        </div>
+        {/* Linux Section */}
+        <div className={styles.item3}>
+          <div className={styles.android}>
+            <div className={styles.androidText}> Android</div>
+            <br />
+            <a className={styles.androidDownload}>Download</a>
+            <div>
+              <img
+                className={styles.androidImage}
+                src="ios.svg"
+                alt="ios-image"
+              ></img>
+            </div>
+          </div>
+        </div>
+        <div className={styles.item4}>Box 4</div>
+      </div>
+    </div>
+  );
 }
